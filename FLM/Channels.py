ddcd6de4523353d67a9ce4018e706c4166e8822f
Max <maxworsle@outlook.com>
2022-02-15 12:55:19 +0000

    Refactor into new connection classes for client and server, pass server socket in

--- FLM/Channels.py
+++ FLM/Channels.py
@@ -7,6 +7,7 @@ class BaseChannel:
 
     def disconnect(self):
         self.connection.disconnect()
+        self.connection = None
 
     def send(self, message):
         serialised_message = Serialisation.Serialiser.serialise_message(message)
@@ -24,20 +25,21 @@ class BaseChannel:
         return deserialised_message
 
     def __del__(self):
-        self.connection.disconnect()
+        if self.connection is not None:
+            self.connection.disconnect()
 
 
 class ChannelToServer(BaseChannel):
     def __init__(self, local_port):
-        self.connection = Connection.Connection(local_port=local_port, is_server=False)
+        self.connection = Connection.ConnectionToServer("", local_port)
 
     def establish_connection(self, target_ip, target_port):
         self.connection.connect_to_remote(target_ip, target_port)
 
 
 class ChannelToClient(BaseChannel):
-    def __init__(self, local_port, local_ip):
-        self.connection = Connection.Connection(local_ip=local_ip, local_port=local_port, is_server=True)
+    def __init__(self, local_socket):
+        self.connection = Connection.ConnectionToClient(local_socket)
 
     def establish_connection(self):
         self.connection.wait_for_connection()
--- FLM/ChannelsTest.py
+++ FLM/ChannelsTest.py
@@ -1,19 +1,29 @@
 import unittest
 import Channels
+import Connection
 import MessageDefinitions
 import threading
 
 
 class ChannelUsage(unittest.TestCase):
     def test_connection(self):
-        client_channel_to_server, server_channel_to_client = self.make_client_server_channels(40400)
+        server_port = 40400
+        new_socket = Connection.get_new_server_socket("", server_port)
+        client_channel_to_server, server_channel_to_client = self.make_client_server_channels(new_socket, server_port)
+
         self.connect_client_server(server_channel_to_client, client_channel_to_server, 40400)
-        self.assertTrue(client_channel_to_server.connection.socket is not None)
+
+        self.assertTrue(client_channel_to_server.connection is not None)
         client_channel_to_server.disconnect()
-        self.assertTrue(client_channel_to_server.connection.socket is None)
+        self.assertTrue(client_channel_to_server.connection is None)
+
+        new_socket.close()
 
     def test_send_message_to_server(self):
-        client_channel_to_server, server_channel_to_client = self.make_client_server_channels(40400)
+        server_port = 40400
+        new_socket = Connection.get_new_server_socket("", server_port)
+        client_channel_to_server, server_channel_to_client = self.make_client_server_channels(new_socket, server_port)
+
         self.connect_client_server(server_channel_to_client, client_channel_to_server, 40400)
 
         sent_message = MessageDefinitions.BaseMessage(1, 1, 0, 12000)
@@ -21,13 +31,15 @@ class ChannelUsage(unittest.TestCase):
         received_message = server_channel_to_client.sync_receive()
 
         self.assertTrue(sent_message.__repr__() == received_message.__repr__())
+        new_socket.close()
 
     @staticmethod
     def connect_client_server(server_channel_to_client, client_channel_to_serer, server_port):
         try:
             server_thread = threading.Thread(target=server_channel_to_client.establish_connection)
             server_thread.start()
-            client_thread = threading.Thread(target=client_channel_to_serer.establish_connection, args=("127.0.0.1", server_port))
+            client_thread = threading.Thread(target=client_channel_to_serer.establish_connection,
+                                             args=("127.0.0.1", server_port))
             client_thread.start()
 
             client_thread.join()
@@ -38,9 +50,9 @@ class ChannelUsage(unittest.TestCase):
             raise e
 
     @staticmethod
-    def make_client_server_channels(server_port):
+    def make_client_server_channels(server_socket, server_port):
         client_channel_to_server = Channels.ChannelToServer(server_port + 1)
-        server_channel_to_client = Channels.ChannelToClient(server_port, "")
+        server_channel_to_client = Channels.ChannelToClient(server_socket)
 
         return client_channel_to_server, server_channel_to_client
 
