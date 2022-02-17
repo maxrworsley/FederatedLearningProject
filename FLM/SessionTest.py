8c5f5ff8b6b04bc2986421780a79ab16faf9bada
Max <maxworsle@outlook.com>
2022-02-17 15:12:54 +0000

    Add sending test to session unit tests

--- FLM/SessionTest.py
+++ FLM/SessionTest.py
@@ -13,7 +13,7 @@ class SessionManagerTests(unittest.TestCase):
     def test_session_connection(self):
         c_send_queue, c_receive_queue = queue.Queue(), queue.Queue()
         s_send_queue, s_receive_queue = queue.Queue(), queue.Queue()
-        s_sock_num = 40402
+        s_sock_num = 40400
         new_socket = Connection.get_new_server_socket("", s_sock_num)
         client_session = Session.ClientSessionManager(c_send_queue, c_receive_queue, s_sock_num + 1, "127.0.0.1", s_sock_num)
         server_session = Session.ServerSessionManager(s_send_queue, s_receive_queue, new_socket)
@@ -23,15 +23,38 @@ class SessionManagerTests(unittest.TestCase):
         server_thread.start()
         client_thread.start()
 
-        c_send_queue.put(MessageDefinitions.BaseMessage(0, 0, 1, 0))
-        self.get_message_from_session(s_receive_queue)
-        s_send_queue.put(MessageDefinitions.BaseMessage(0, 1, 0, 1))
-        self.get_message_from_session(c_receive_queue)
-
         c_send_queue.put(MessageDefinitions.StopSession(0, 0, 0, 0))
         client_thread.join()
         server_thread.join()
         self.assertTrue(True)
+        new_socket.close()
+
+    def test_session_send(self):
+        c_send_queue, c_receive_queue = queue.Queue(), queue.Queue()
+        s_send_queue, s_receive_queue = queue.Queue(), queue.Queue()
+        s_sock_num = 40402
+        new_socket = Connection.get_new_server_socket("", s_sock_num)
+        client_session = Session.ClientSessionManager(c_send_queue, c_receive_queue, s_sock_num + 1, "127.0.0.1",
+                                                      s_sock_num)
+        server_session = Session.ServerSessionManager(s_send_queue, s_receive_queue, new_socket)
+
+        client_thread = threading.Thread(target=client_session.start)
+        server_thread = threading.Thread(target=server_session.start)
+        server_thread.start()
+        client_thread.start()
+
+        first_message_sent = MessageDefinitions.BaseMessage(0, 0, 1, 0)
+        c_send_queue.put(first_message_sent)
+        first_message_received = self.get_message_from_session(s_receive_queue)
+        second_message_sent = MessageDefinitions.BaseMessage(0, 1, 0, 1)
+        s_send_queue.put(second_message_sent)
+        second_message_received = self.get_message_from_session(c_receive_queue)
+
+        c_send_queue.put(MessageDefinitions.StopSession(0, 0, 0, 0))
+        client_thread.join()
+        server_thread.join()
+        self.assertTrue(first_message_sent.__repr__() == first_message_received.__repr__())
+        self.assertTrue(second_message_sent.__repr__() == second_message_received.__repr__())
         new_socket.close()
 
     @staticmethod
@@ -43,7 +66,7 @@ class SessionManagerTests(unittest.TestCase):
             except _queue.Empty:
                 pass
 
-        print(message)
+        return message
 
 if __name__ == '__main__':
     unittest.main()
