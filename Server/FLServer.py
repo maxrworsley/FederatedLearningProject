48f9a40c1c2ca19ccf66ac4f96b938311749fe53
Max <maxworsle@outlook.com>
2022-02-21 09:31:30 +0000

    Removed unnecessary functionality from server

--- Server/FLServer.py
+++ Server/FLServer.py
@@ -1,12 +1,10 @@
 import ServerTensorflowHandler
-import DataWrapper
 
 
 class FLServer:
     def __init__(self, coordinator):
         self.coordinator = coordinator
-        data_wrapper = DataWrapper.DataWrapper("/home/max/Documents/FederatedLearning/Data/data.csv")
-        tf_handler = ServerTensorflowHandler.TensorflowHandler("BasicHandler", data_wrapper)
+        tf_handler = ServerTensorflowHandler.TensorflowHandler()
         self.coordinator.set_handler(tf_handler)
 
     def run_server(self):
