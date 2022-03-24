d4312a2be053cc67ec7bbe56dfaae8d5f92b6bff
Max <maxworsle@outlook.com>
2022-03-24 18:31:50 +0000

    Complete nodewrapper functionality

---
+++ Server/NodeWrapper.py
@@ -0,0 +1,28 @@
+from FLM import Session
+from FLM import MessageDefinitions
+import queue
+import threading
+
+
+class NodeWrapper:
+    send_queue = None
+    receive_queue = None
+    server_session = None
+    session_thread = None
+
+    def __init__(self, local_socket):
+        self.send_queue, self.receive_queue = queue.Queue(), queue.Queue()
+        self.server_session = Session.ServerSessionManager(self.send_queue, self.receive_queue, local_socket)
+
+    def start(self):
+        self.session_thread = threading.Thread(target=self.server_session.start)
+        self.session_thread.start()
+
+    def send(self, message):
+        self.send_queue.put(message)
+
+    def receive(self, block=False):
+        return self.receive_queue.get(block=block)
+
+    def stop_premature(self):
+        self.send_queue.put(MessageDefinitions.StopSession())
