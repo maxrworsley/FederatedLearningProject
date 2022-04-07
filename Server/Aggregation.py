3977a629bb53009f2ccfd1f606470672d20fabee
Max <maxworsle@outlook.com>
2022-04-07 10:08:49 +0100

    Add aggregation function for two models

--- Server/Aggregation.py
+++ Server/Aggregation.py
@@ -1,3 +1,8 @@
+import copy
+
+import numpy as np
+
+
 class ModelAggregationHandler:
     models = []
     scores = []
@@ -13,3 +18,18 @@ class ModelAggregationHandler:
 
         return self.models[index_min]
 
+    @staticmethod
+    def average_two_models(model_1, model_2):
+        weights_model_1 = np.array(model_1.get_weights(), dtype=object)
+        weights_model_2 = np.array(model_2.get_weights(), dtype=object)
+
+        new_weights = []
+        for i in range(len(weights_model_1)):
+            first_model_row = weights_model_1[i]
+            second_model_row = weights_model_2[i]
+            new_row = (first_model_row + second_model_row) / 2.0
+            new_weights.append(new_row)
+
+        new_model = copy.deepcopy(model_1)
+        new_model.set_weights(new_weights)
+        return new_model
