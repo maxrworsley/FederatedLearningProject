b2d655699003eaa3cff56d0842514d16970e3c99
Max <maxworsle@outlook.com>
2022-04-07 13:19:38 +0100

    Add visualisation class with skeleton methods

---
+++ Server/Visualisation.py
@@ -0,0 +1,12 @@
+from matplotlib import pyplot
+
+
+class Visualiser:
+    @staticmethod
+    def plot_history_over_epochs(history):
+        pass
+
+    @staticmethod
+    def plot_evaluation_losses(losses):
+        pass
+
