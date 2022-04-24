821226bfe312075055402b63d086ae403e83b59d
Max <maxworsle@outlook.com>
2022-04-24 16:18:35 +0100

    Build script functioning

--- build_nodes.py
+++ build_nodes.py
@@ -11,17 +11,17 @@ def build_nodes():
                   for file in os.listdir(data_path)
                   if os.path.isfile(os.path.join(data_path, file))]
     print(only_files)
-    lines = ["#!/bin/bash"]
+    lines = ["#!/bin/bash\n"]
 
     i = 0
     for file in only_files:
         i += 1
         shutil.copy(file, "./Node/data.csv")
-        subprocess.run(f"docker build -t fls_node:1.{i} .")
+        subprocess.run(f"docker build -t fls_node:1.{i} .", shell=True)
         os.remove("./Node/data.csv")
         lines.append(f"gnome-terminal --tab --title=\"Node_{i}\" "
                      f"--command=\"bash -c 'docker run -it --rm -p "
-                     f"127.0.0.1:4030{i}:40400 fls_node:1.{i}; echo $SHELL'\"")
+                     f"127.0.0.1:4030{i}:40400 fls_node:1.{i}; echo $SHELL'\"\n")
 
     with open("./launch_nodes.sh", "w") as launch_node_file:
         launch_node_file.writelines(lines)
@@ -33,7 +33,6 @@ if __name__ == '__main__':
     try:
         build_nodes()
     finally:
-        print("Error occured in script. Stopping")
         try:
             os.remove("./Node/data.csv")
         except OSError:
